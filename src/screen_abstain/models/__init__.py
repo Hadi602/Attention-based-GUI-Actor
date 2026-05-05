6cc9fa488fdbada9307d73c2519c23185078aa2d
Rashid <126160856+Hadi602@users.noreply.github.com>
2026-05-05 18:03:46 +0800

    Initialize __init__.py with model imports

---
+++ src/screen_abstain/models/__init__.py
@@ -0,0 +1,8 @@
+"from .action_head_with_null import VisionHead_MultiPatchWithNull
+from .patched_model import Qwen2VLForConditionalGenerationWithNullPointer
+
+__all__ = [
+    \"VisionHead_MultiPatchWithNull\",
+    \"Qwen2VLForConditionalGenerationWithNullPointer\",
+]
+"
